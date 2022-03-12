4aa44e9ce00a0710c36c693688a67cc53ea8a834
JoaoPedroCordoba <jp.8pedro86@gmail.com.br>
2022-03-11 22:37:19 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string nome, sobrenome, SOBRENOME;
+
+Console.WriteLine("digite seu primeiro nome:");
+nome=Console.ReadLine()!;
+
+Console.WriteLine("digite seu sobrenome:");
+sobrenome=Console.ReadLine()!;
+
+SOBRENOME=sobrenome.ToUpper();
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}"!);
+Console.WriteLine($"Nome de catalogo: {SOBRENOME} {nome} "!);
